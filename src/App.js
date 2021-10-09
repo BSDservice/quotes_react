4a9137b83e2c5a9ced52789a0283d6459b2dc3b7
Aleksandr Krysov <kpblcbl4@gmail.com>
2021-10-09 13:23:47 +0300

    ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Grid} from "@material-ui/core";
-import CardCite from './CardCite'
+import CardCite from './CardCite';
 
 const colors = [
   '#16a085',
@@ -15,18 +15,71 @@ const colors = [
   '#BDBB99',
   '#77B1A9',
   '#73A857'];
+
+var quotes = [
+
+  {
+         "quote":"Life isn’t about getting and having, it’s about giving and being.","author":"Kevin Kruse"},
+  {
+         "quote":"Whatever the mind of man can conceive and believe, it can achieve.","author":"Napoleon Hill"},
+  {
+         "quote":"Strive not to be a success, but rather to be of value.","author":"Albert Einstein"},
+  {
+         "quote":"Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.","author":"Robert Frost"},
+  {
+         "quote":"I attribute my success to this: I never gave or took any excuse.","author":"Florence Nightingale"},
+  {
+         "quote":"You miss 100% of the shots you don’t take.","author":"Wayne Gretzky"},
+  {
+         "quote":"I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.","author":"Michael Jordan"},
+  {
+         "quote":"The most difficult thing is the decision to act, the rest is merely tenacity.","author":"Amelia Earhart"},
+  {
+         "quote":"Every strike brings me closer to the next home run.","author":"Babe Ruth"},
+  {
+         "quote":"Definiteness of purpose is the starting point of all achievement.","author":"W. Clement Stone"},
+  {
+         "quote":"We must balance conspicuous consumption with conscious capitalism.","author":"Kevin Kruse"},
+  {
+         "quote":"Life is what happens to you while you’re busy making other plans.","author":"John Lennon"},
+  {
+         "quote":"We become what we think about.","author":"Earl Nightingale"}];
+
+fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json", {
+    "method": "GET",
+    "headers": {
+      Accept: 'application/json'
+    }
+  })
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    quotes = data.quotes;
+  })
+  .catch(err => {
+    console.error(err);
+  });
   
 class App extends React.Component {
+  
   constructor(props){
     super(props);
+    let startCite = quotes[Math.floor(Math.random()*Number(quotes.length))]
     this.state = {
-      cite: 'Это цитата Великого Человека',
-      author: 'Великий Человек',
+      cite: startCite.quote,
+      author: startCite.author,
       color: colors[Math.floor(Math.random()*12)]
     }
+    this.quotesGen = function*(){
+      for (let quote of quotes) {
+        let color = colors[Math.floor(Math.random()*12)]
+        yield [quote, color]
+      }
+    }()
     this.nextCite = this.nextCite.bind(this)
-    this.componentDidMount = this.componentDidMount.bind(this)
   }
+  
 
   nextCite(){
       let arr = this.quotesGen.next().value
@@ -38,31 +91,8 @@ class App extends React.Component {
         color: color
       })
   }
-
-  componentDidMount(){
-    fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json", {
-        "method": "GET",
-        "headers": {
-          Accept: 'application/json'
-        }
-      })
-      .then(response => {
-        return response.json();
-      })
-      .then(data => App.quotes = data.quotes)
-      .catch(err => {
-        console.error(err);
-      });
-
-      this.quotesGen = function*(){
-        for (let quote of App.quotes) {
-          let color = colors[Math.floor(Math.random()*12)]
-          yield [quote, color]
-        }
-      }()    
-  }
+  
   render(){
-    console.log(Math.random()*12 ,this.state.color)
     return (      
         <Grid
           container
--- src/CardCite.js
+++ src/CardCite.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {Facebook, Twitter, FormatQuoteRounded} from '@material-ui/icons';
+import {Twitter} from '@material-ui/icons';
 import {IconButton, Button, Typography, CardContent, CardActions, Card} from '@material-ui/core';
 
 const useStyles = makeStyles({
@@ -25,30 +25,27 @@ export default function CardCite(props) {
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
+    <Card id="quote-box" className={classes.root}>
       <CardContent>
         <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
+          Слово Дня
         </Typography>
-        <Typography variant="h4" component="blockquote" style={{color: props.color, transitionDuration: '1s',
+        <Typography id="text" variant="h5" component="blockquote" style={{color: props.color, transitionDuration: '1s',
                    transitionProperty: 'color'}}>
             {props.cite}
         </Typography>
-        <Typography className={classes.autor} variant="h6" component="figcaption" color="textSecondary">
+        <Typography id="author" className={classes.autor} variant="h6" component="figcaption" color="textSecondary">
           -<cite>{props.author}</cite>
         </Typography>
       </CardContent>
       <CardActions className={classes.buttons}>
         <div style={{textAlign:"left"}}>        
-        <IconButton>
+        <IconButton id="tweet-quote" component="a" href="https://twitter.com/intent/tweet" target="_blank">
             <Twitter fontSize="large"></Twitter>
         </IconButton>
-          <IconButton>
-            <Facebook fontSize="large"></Facebook>
-          </IconButton>
         </div>
         <div style={{textAlign:"right"}}>
-          <Button variant="contained" size="small" onClick={props.nextCite} color='primary'>New quote</Button>
+          <Button id="new-quote" variant="contained" size="small" onClick={props.nextCite} color='secondary'>New quote</Button>
         </div>
       </CardActions>
     </Card>
